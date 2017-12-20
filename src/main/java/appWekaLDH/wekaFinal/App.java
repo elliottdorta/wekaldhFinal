ff6cd38cdf37f790a8ce667aba27f2604896540e
Elliott Dorta <elliottdr91@gmail.com>
2017-12-20 11:44:41 +0000

    Base proyecto

---
+++ src/main/java/appWekaLDH/wekaFinal/App.java
@@ -0,0 +1,13 @@
+package appWekaLDH.wekaFinal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
