db8483bb6a6e38a13079618008eae8d5bc91bb3a
alu0100207231 <alu0100207231@ull.edu.es>
2017-12-22 09:10:51 +0000

    Añadiendo Test Regresión Simple

---
+++ src/test/java/appWekaLDH/wekaFinal/SimpleLinearRegressionTest.java
@@ -0,0 +1,49 @@
+package appWekaLDH.wekaFinal;
+
+import java.io.File;
+
+import junit.framework.TestCase;
+
+
+/**
+ * Test simple de la clase LinearRegressionWeka
+ * @author HilarioJ
+ *
+ */
+
+public class SimpleLinearRegressionTest extends TestCase {
+	
+	/**
+	 * Comprueba que el fichero de datos existe
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 * @throws ClassNotFoundException
+	 */
+	public final void testFileExists() throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+		File file = new File("datasheets/cpu.arff");
+		assertTrue(file.exists());
+	}
+
+	/**
+	 * Comprueba que no falle la ejecucion del metodo al importar el fichero
+	 * @param datasheet cpu.arff
+	 */
+	public final void testSimpleLinearRegressionWeka() {
+		File file_import = new File("datasheets/cpu.arff");
+		SimpleLinearRegressionWeka tester = new SimpleLinearRegressionWeka(file_import);
+		assertNotNull(tester);
+	}
+	
+	/**
+	 * Metodo para ver si el resultado al mostrar por pantalla tiene contenido distinto del defininido en la clase como predeterminado
+	 * @throws Exception
+	 * @param datasheet cpu.arff
+	 */
+	public final void testPrintResult() throws Exception {
+		File file_import = new File("datasheets/cpu.arff");
+		SimpleLinearRegressionWeka tester = new SimpleLinearRegressionWeka(file_import);	
+		String resultado = tester.PrintResult();
+		assertTrue(resultado != "Simple Linear Regression\n");	
+	}
+
+}
